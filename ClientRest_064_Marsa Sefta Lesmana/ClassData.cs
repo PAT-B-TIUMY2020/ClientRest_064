5bdbe43023a3c94d0223cd08ec7d37fe3a1aa72b
Marsasefta <marsasefta01@gmail.com>
2020-12-21 21:34:24 +0700

    Client done

---
+++ ClientRest_064_Marsa Sefta Lesmana/ClassData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static ClientRest_064_Marsa_Sefta_Lesmana.Program;
+
+namespace ClientRest_064_Marsa_Sefta_Lesmana
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1412/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
